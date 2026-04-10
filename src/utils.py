e72a5d26706905fba9978f87c2b21de7ee0cd1e9
Vinay Karakavalasa <vinaykarakavalasa@Vinays-MacBook-Air.local>
2026-04-10 20:12:19 +0530

    updated utils

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from sklearn.metrics import r2_score
+
 from src.logger import logging
 from src.exception import CustomException
 
@@ -25,3 +27,23 @@ def save_object(file_path: str, obj: object) -> None:
     except Exception as e:
         raise CustomException(e, sys)
     
+
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict) -> dict:
+    '''
+    This function is responsible for evaluating the models and returning the report containing the r2 score of each model.
+    '''
+    logging.info("Entered the evaluate_models method of utils")
+    try:
+        report: dict = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
